3cd04c481cecc9c67d7d7f9e8103be0223979ae1
Maor123344 <maor.palt@gmail.com>
2022-07-14 16:33:44 +0300

    homework lesson three Maor

---
+++ homeworkone.py
@@ -0,0 +1,13 @@
+mystring ="Maor Tal Alon"
+
+my_list = mystring.split()
+# print(my_list)
+
+for str in my_list:
+    print(str, ", good morning")
+
+for j in my_list:
+    print(j, ", good night")
+    
+    
+    
